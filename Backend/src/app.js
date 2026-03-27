fe5147ca66f107ed8b9267c3a24ba41caff7c74e
Ayan281 <ayankhan987456@gmail.com>
2026-03-27 22:45:39 +0530

    Allow CORS for multiple origins

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -9,8 +9,11 @@ const cors = require("cors");
 
 const app = express();
 app.use(cors({
-  origin: "http://localhost:5173", 
-  credentials: true                
+  origin: [
+    "http://localhost:5173",
+    "https://spotify-clone-gules-mu.vercel.app"
+  ],
+  credentials: true
 }));
 app.use(express.json());
 app.use(cookieParser());
